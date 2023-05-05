c1696d4c18c78ae969215a897c1c2b7121349b6c
rosedoucette <rose.doucette94@gmail.com>
2023-05-05 14:02:13 -0400

    yup.

--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,47 @@
-fetch (`http://localhost:3000/api/products`).then (res => res.json()) .then (data => {
-const container = document.getElementById (`items`)   
-data.forEach(product => {
-        console.log (product)
-        const productel=`<a href="./product.html?id=${product._id}">
+fetch(`http://localhost:3000/api/products`).then(res => res.json()).then(data => {
+    const container = document.getElementById(`items`)
+    data.forEach(product => {
+        console.log(product)
+        const productel = `<a href="./product.html?id=${product._id}">
         <article>
           <img src="${product.imageUrl}" alt="${product.altTxt}">
           <h3 class="productName">${product.name}</h3>
           <p class="productDescription">${product.description}</p>
         </article>
       </a>`;
-      container.innerHTML += productel
+        container.innerHTML += productel
     });
 })
-console.log (data)
+console.log(data)
+
+
+
+//below is the item configuration for the cart products..
+
+fetch(`http://localhost:3000/api/products`).then(res => res.json()).then(data => {
+    const container = document.getElementById(`items`)
+    data.forEach(product => {
+        console.log(product)
+        const productel = `<article class="cart__item" data-id="${product._id}" data-color="${product.colors}">
+                <div class="cart__item__img">
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${product.name}</h2>
+                    <p>${color}</p>
+                    <p>${product.price}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Quantity : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Delete</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`;
+    });
+})
