6eee6cf39b3787b0d7a3007cc1035cb84888db19
rosedoucette <rose.doucette94@gmail.com>
2023-05-10 16:23:20 -0400

    post-session updates

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,11 @@
+const cart = JSON.parse (localStorage.getItem ("cart") || "[]")
+
 const productId = (new URLSearchParams(window.location.search)).get("id"); //getting the product ID from console.log//
 console.log (productId)
+let productData = {}
 fetch(`http://localhost:3000/api/products/${productId}`).then(res => res.json()).then(product => {
-  const productImg = document.querySelector (".item__img img")
+productData = {...product}  
+const productImg = document.querySelector (".item__img img")
   productImg.src = product.imageUrl;
   productImg.alt = product.altTxt;
   document.getElementById ('title').innerHTML = product.name; //changing element info//
@@ -13,19 +17,38 @@ fetch(`http://localhost:3000/api/products/${productId}`).then(res => res.json())
     colorsElement.innerHTML += colorElement //changes the color options 
   });
 
+  
+
   //below is my attempt at adding products to the cart//
   //might work, you don't know//
 
-  var addItemToCartButton = document.getElementsByClassName ('item__content__addButton')
+  var addItemToCartButton = document.getElementById ('addToCart')
   console.log(addItemToCartButton)
-  for (var i = 0; i > addItemToCartButton; i++) {
-    var button = addItemToCartButton[i]
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.add ()
-        updateCartTotal()
+    addItemToCartButton.addEventListener('click', function(event) {
+       const productColor = document.getElementById ('colors').value 
+       if (!productColor) { //! is like opposite. so if there's no product color selected//
+        alert ("Please select a color")
+        return 
+       }
+       const productQuantity = Number(document.getElementById ('quantity').value) //Numbers clarifies that we are changing this info to a number//
+       if (!productQuantity || Number.isNaN(productQuantity)) {
+        alert ("Please add a quantity")
+        return //return tells the code to stop running any further and return/display the message..
+       }
+        const productMatching = cart.find ((product) => product.id === productId && product.color === productColor)
+        if (productMatching) {
+            productMatching.quantity += productQuantity 
+        }
+        else {
+            cart.push ({
+                ...productData, //pulling down product copy & avoids extra product calls on the following page//
+                id:productId, color:productColor, quantity:productQuantity
+            })
+        }
+        localStorage.setItem ("cart", JSON.stringify(cart))
+        alert ("Cart updated!")
     })
-  }
+  
 
   function updateCartTotal() {
     var cartItemContainer = document.getElementById('cart__items')[0]
