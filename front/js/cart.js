00e3792e5d75f691ed823d63bd5b65d0b9baa254
rosedoucette <rose.doucette94@gmail.com>
2023-05-11 12:43:39 -0400

    template literals for cart.js

---
+++ front/js/cart.js
@@ -0,0 +1,27 @@
+(res => res.json()).then(data => {
+    const container = document.getElementById(`items`)
+    data.forEach(product => {
+        const productel = `<article class="cart__item" data-id="${product._id}" data-color="${product.colors}">
+            <div class="cart__item__img">
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                    <h2>${product.name}</h2>
+                    <p>${product.colors}</p>
+                    <p>€${product.price}</p>
+                </div>
+                <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                        <p>Quantity : </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Delete</p>
+                    </div>
+                </div>
+            </div>
+        </article>`;
+        container.innerHTML += productel
+    });
+})
