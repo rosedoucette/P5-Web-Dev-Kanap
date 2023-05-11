735cc4ef33194f10d5a5c6b84009479ab25ebac9
rosedoucette <rose.doucette94@gmail.com>
2023-05-11 13:51:57 -0400

    some tweaks

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,7 @@
-(res => res.json()).then(data => {
-    const container = document.getElementById(`items`)
-    data.forEach(product => {
-        const productel = `<article class="cart__item" data-id="${product._id}" data-color="${product.colors}">
+const cart = JSON.parse(localStorage.getItem("cart") || "[]")
+const container = document.getElementById(`items`)
+cart.forEach(product => {
+    const productel = `<article class="cart__item" data-id="${product._id}" data-color="${product.colors}">
             <div class="cart__item__img">
                 <img src="${product.imageUrl}" alt="${product.altTxt}">
             </div>
@@ -13,7 +13,7 @@
                 </div>
                 <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                        <p>Quantity : </p>
+                        <p>${product.quantity} </p>
                         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
                     </div>
                     <div class="cart__item__content__settings__delete">
@@ -22,6 +22,4 @@
                 </div>
             </div>
         </article>`;
-        container.innerHTML += productel
-    });
-})
+});
