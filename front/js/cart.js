ba92e9b3948c64b4423abe29fa6cbaf60487de43
rosedoucette <rose.doucette94@gmail.com>
2023-06-14 15:04:36 -0400

    attempting to update cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -39,17 +39,48 @@ var itemQuantity = document.getElementsByClassName('itemQuantity')
 for (let i = 0; i < itemQuantity.length; i += 1) {
     itemQuantity[i].addEventListener('change', myFunction);
 }
-var cartTotal = Array.from(document.getElementsByClassName('cart__price')) 
-cartTotal.forEach (function (total){ total.addEventListener('change', myFunction)});
+var cartTotal = Array.from(document.getElementsByClassName('cart__price'))
+cartTotal.forEach(function (total) { total.addEventListener('change', myFunction) });
 
 function myFunction(e) {
-    const {target} = e 
-    console.log (target.value)
+    const { target } = e
+    console.log(target.value)
     var parent = target.closest('article.cart__item')
-    const {id, color} = parent.dataset
-    console.log (id, color);
-
+    const { id, color } = parent.dataset
+    console.log(id, color);
 }
+
+var updateCart = Array.from(document.getElementsByTagName('totalPrice'))
+updateCart.forEach(function (total) {total.addEventListener('change', myFunction) });
+
+function myFunction(e) {
+    const { target } = e
+    var parent = target.closest('input.itemQuantity')
+    const { id, color } = parent.dataset;
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+function refreshPage() {
+    window.location.reload(false);
+  }
+
+<'itemQuantity' onchange={() => window.location.reload(false)}>Click to reload!</'itemQuantity'>
+
+// var updateCart = document.getElementsByClassName('itemQuantity')
+// updateCart.addEventListener('click', function (event) {
+//     const productMatching = cart.find((product) => product.id === productId && product.color === productColor)
+//     if (productMatching) {
+//         productMatching.quantity += productQuantity //not sure if this part is applicable//
+//     }
+//     else {
+//         cart.push({
+//             ...productData, //pulling down product copy & avoids extra product calls on the following page//
+//             id: productId, color: productColor, quantity: productQuantity
+//         })
+//     }
+//     localStorage.setItem("cart", JSON.stringify(cart))
+//     alert("Cart updated!")
+// })
 
 //copied from product.js, may not be fully applicable...unless change quantity to price?:
 //const productQuantity = Number(document.getElementById('quantity').value)
